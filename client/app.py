b8bca5e0e66fd1aa484c42fc5a908df35dba30ba
jo <jo@.com>
2025-02-22 12:47:32 +0000

    three.jsを使用するためのクライアントの変更

--- client/app.py
+++ client/app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    model_file = request.args.get(
+        "model", "models/tooth-data-T2-14.stl"
+    )  # `static/models/` に移動
+    return render_template("index.html", model_file=model_file)
 
 
 if __name__ == "__main__":
